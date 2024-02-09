b6e1900f4dbfda282a2d909524a743af0054678c
letItCurl <rolandlopez.developer@gmail.com>
2024-02-09 08:53:27 +0700

    Base app.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_09_011844) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_09_013419) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,4 +20,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_09_011844) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "notes", force: :cascade do |t|
+    t.text "content"
+    t.bigint "customer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_notes_on_customer_id"
+  end
+
+  add_foreign_key "notes", "customers", on_delete: :cascade
 end
